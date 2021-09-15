ffb87bd12cc1452c41e4356dabdfb3af7642c362
lynx-boop <shimukurumhi@gmail.com>
2021-09-15 15:49:23 +0300

    начинает подготавливать иконки
    Signed-off-by: lynx-boop <shimukurumhi@gmail.com>

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,19 @@
-let toggle = document.querySelector('.main-nav__toggle');
+const toggle = document.querySelector('.main-nav__toggle');
+const menu = document.querySelector('.main-nav__list');
+
+toggle.classList.remove('main-nav__toggle--no-js');
+menu.classList.remove('main-nav__list--no-js');
 
 toggle.onclick = function () {
-  toggle.classList.toggle('toggle-open');
-  toggle.classList.toggle('toggle-close');
-};
+  if (toggle.classList.contains('main-nav__toggle--close')) {
+    toggle.classList.remove('main-nav__toggle--close');
+    toggle.classList.add('main-nav__toggle--open');
+    menu.classList.remove('main-nav__list--close');
+    menu.classList.add('main-nav__list--open');
+  } else {
+    toggle.classList.add('main-nav__toggle--close');
+    toggle.classList.remove('main-nav__toggle--open');
+    menu.classList.add('main-nav__list--close');
+    menu.classList.remove('main-nav__list--open');
+  }
+}
